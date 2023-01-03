8cbb1d09593049963f750826e3d51d182500821d
syahidanAS <syahidan139@gmail.com>
2023-01-03 11:08:05 +0700

    feat: add homepage

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,41 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+
+<div class="flex flex-col">
+    <div class="py-36 w-1/2 mx-auto">
+<div class="flex flex-row items-center gap-8">
+    <div class="flex flex-wrap">
+        <div class="flex flex-row items-center mx-auto border-4 border-slate-800 rounded-full p-1">
+            <img class="w-[500px] rounded-full overflow-hidden" src="@/assets/images/ava.png" alt="ava">
+        </div>
+
+    </div>
+    <div class="flex flex-col gap-2">
+        <h1 class="text-4xl font-bold text-slate-700">Syahidan Arrizaldy Sidik</h1>
+        <h2 class="text-xl font-semibold text-slate-700">Front End Engineer From Indonesia</h2>
+        <p class="text-base font-normal text-slate-600 text-justify">Hi there, i'm a front end engineer from West Java, Indonesia. also that I really love coffee to provide energy in carrying out activities ☕</p>
+</div>
+</div>
+</div>
+
+<div class="py-32 w-10/12 mx-auto ">
+<div class="flex flex-row-reverse items-center gap-20 ">
+    <div class="flex flex-wrap ">
+        <div class="flex flex-row items-center mx-auto">
+            <img class="w-[1000px]  rounded-md" src="@/assets/images/tlc.png" alt="ava">
+        </div>
+    </div>
+    <div class="flex flex-col gap-2">
+        <h1 class="text-4xl font-bold text-slate-700 my-4">Last project I worked on</h1>
+        <a class="text-xl font-semibold text-slate-700 hover:text-sky-900" href="https://thebrightcourse.com" target="_blank">The Bright Learning Center</a>
+        <p class="text-base font-normal text-slate-600 text-justify">The Bright Learning Center adalah Lembaga Bimibingan Belajar dan Kursus Bahasa Asing yang terletak di Karawang, Jawa Barat Indonesia</p>
+</div>
+</div>
+</div>
+</div>
+
+
 </template>
+
+<script>
+
+</script>
