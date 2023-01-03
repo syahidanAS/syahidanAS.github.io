023f72491f30cb417573b17af2382d2b1fbfcaa5
syahidanAS <syahidan139@gmail.com>
2023-01-03 15:07:15 +0700

    feat: add projects

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -29,7 +29,7 @@
           <h2 class="text-xl font-semibold text-slate-700">
             Front End Engineer From Indonesia
           </h2>
-          <p class="text-base font-normal text-slate-600 mr-6">
+          <p class="text-base font-normal text-slate-600 mr-0 lg:mr-6">
             Hi there, i'm a front end engineer from West Java, Indonesia. also
             that I really love coffee to provide energy in carrying out
             activities ☕
@@ -38,13 +38,11 @@
       </div>
     </div>
 
-    <div class="py-14 lg:py-20 w-10/12 mx-auto">
+    <div class="flex flex-col gap-4 py-14 lg:py-20 w-10/12 mx-auto">
       <div
-        class="flex flex-col lg:flex-row-reverse items-center gap-4 lg:gap-20">
+        class="flex flex-col lg:flex-row-reverse items-center gap-4 lg:gap-20"
+      >
         <div class="flex flex-wrap">
-          <h1 class="text-2xl font-bold text-slate-700 my-4 md:hidden">
-            Last project I worked on
-          </h1>
           <div class="flex flex-row items-center mx-auto">
             <img
               class="w-[1000px] rounded-md"
@@ -54,30 +52,18 @@
           </div>
         </div>
         <div class="flex flex-col gap-2">
-          <h1
-            class="
-              text-4xl
-              font-bold
-              text-slate-700
-              my-4
-              invisible
-              hidden
-              lg:visible lg:block
-            "
-          >
-            Last project I worked on
-          </h1>
           <a
-            class="text-base font-semibold text-slate-700 hover:text-sky-900"
+            class="text-2xl font-semibold text-slate-700 hover:text-sky-900"
             href="https://thebrightcourse.com"
             target="_blank"
             >The Bright Learning Center</a
           >
-          <p class="text-base font-normal text-slate-600 text-justify">
-            The Bright Learning Center adalah Lembaga Bimibingan Belajar dan
-            Kursus Bahasa Asing yang terletak di Karawang, Jawa Barat Indonesia
+          <p class="text-base font-normal text-slate-600 text-justify max-w-2xl w-full  lg:w-96">
+            Company Profile dilengkapi dengan Content Management System (CMS). Adapun untuk
+            halaman back office memiliki beberapa fitur antara lain registrasi siswa baru, absensi
+            siswa dan tutor, penagihan billing siswa, dan payroll untuk penggajian tutor.
           </p>
-          <router-link
+          <a
             class="
               border-2
               font-semibold
@@ -88,12 +74,56 @@
               hover:bg-slate-700 hover:text-white
               text-center
             "
-            to="/portfolio"
-            >More Projects</router-link
+            href="https://thebrightcourse.com"
+            target="_blank"
+            >Visit Site</a
           >
         </div>
       </div>
-      
+
+      <div
+        class="flex flex-col lg:flex-row-reverse items-center gap-4 lg:gap-20"
+      >
+        <div class="flex flex-wrap">
+          <div class="flex flex-row items-center mx-auto">
+            <img
+              class="w-[1000px] rounded-md"
+              src="@/assets/images/bpr.png"
+              alt="ava"
+            />
+          </div>
+        </div>
+        
+        <!-- TK Bintang Pertiwi -->
+        <div class="flex flex-col gap-2 ">
+          <a
+            class="text-2xl font-semibold text-slate-700 hover:text-sky-900"
+            href="https://tkbintangpertiwi.com/"
+            target="_blank"
+            >TK Bintang Pertiwi</a
+          >
+          <p class="text-base font-normal text-slate-600 text-justify w-full lg:w-96">
+            Company Profile dilengkapi dengan Content Management System (CMS). Adapun untuk
+            halaman back office memeiliki beberapa fitur utama yaitu laporan pembelajaran siswa yang
+            dapat digunakan langsung oleh orang tua siswa.
+          </p>
+          <a
+            class="
+              border-2
+              font-semibold
+              text-slate-700
+              border-slate-700
+              p-2
+              rounded-md
+              hover:bg-slate-700 hover:text-white
+              text-center
+            "
+            href="https://tkbintangpertiwi.com/"
+            target="_blank"
+            >Visit Site</a
+          >
+        </div>
+      </div>
     </div>
 
     <h1 class="text-2xl font-bold text-slate-700 my-4 mx-auto">
@@ -127,7 +157,6 @@
         allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
         loading="lazy"
       ></iframe>
-
     </div>
   </div>
 </template>
